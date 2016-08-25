77b5a6317510990673e2061d64fa59e0806c647f
Henrylle <henryllemaia@gmail.com>
2016-08-25 16:46:45 -0300

    .

--- Program.cs
+++ Program.cs
@@ -11,12 +11,18 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var amazonS3Client = new AmazonS3Client(new InstanceProfileAWSCredentials(), RegionEndpoint.USEast1);
-            var amazonS3Integration = new AmazonS3Integration(amazonS3Client);
-            Task task = (amazonS3Integration.CopiarArquivo("video_profile.mp4", "conteudo-oregon-temp"));
-            task.Start();
-            task.Wait();
-            Console.WriteLine("Hello World!");
+            TestAsyncAwaitMethods();
         }
+
+        public static void TestAsyncAwaitMethods()
+        {
+            var credentials = new InstanceProfileAWSCredentials();
+            var amazonS3Client = new AmazonS3Client(credentials, RegionEndpoint.USEast1);
+            var amazonS3Integration = new AmazonS3Integration(amazonS3Client);
+            Console.WriteLine("OK");
+            Task.Run(() => amazonS3Integration.CopiarArquivo("video_profile.mp4", "conteudo-oregon-temp"));
+            Console.ReadKey();
+        }
+
     }
 }
